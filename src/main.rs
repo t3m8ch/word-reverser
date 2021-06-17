88a8093a58e2b4e4678a02bf93f2ba7f014c4fc5
Kudyakov Artem <t3m8ch@yandex.ru>
2021-06-17 14:18:00 +0400

    fix: Work with multi-line texts

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,19 @@ use std::{io, process};
 use word_reverser as wr;
 
 fn main() {
-    let mut text = String::new();
-    io::stdin()
-        .read_line(&mut text)
-        .unwrap_or_else(|_| {
-            eprintln!("Failed to read line");
-            process::exit(1);
-        });
+    loop {
+        let mut text = String::new();
+        io::stdin()
+            .read_line(&mut text)
+            .unwrap_or_else(|_| {
+                eprintln!("Failed to read line");
+                process::exit(1);
+            });
 
-    println!("{}", wr::flip_text_words(text.as_str()));
+        if text.trim().is_empty() {
+            break;
+        }
+
+        println!("{}", wr::flip_text_words(text.trim()));
+    }
 }
