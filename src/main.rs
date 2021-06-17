40611afebc8ca6b070289790726313fb2f84d85f
Kudyakov Artem <t3m8ch@yandex.ru>
2021-06-17 14:13:48 +0400

    feat: Create a CLI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::{io, process};
+
 use word_reverser as wr;
 
 fn main() {
-    println!("{}", wr::flip_text_words("Hello, world!"));
+    let mut text = String::new();
+    io::stdin()
+        .read_line(&mut text)
+        .unwrap_or_else(|_| {
+            eprintln!("Failed to read line");
+            process::exit(1);
+        });
+
+    println!("{}", wr::flip_text_words(text.as_str()));
 }
